ad5856b606042c3884f8ec48e054573a70f8234c
scum7ck <nitnelav96@mail.ru>
2016-09-17 18:03:07 +0300

    some static

---
+++ test/controllers/store_controller_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class StoreControllerTest < ActionController::TestCase
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_select '#columns #side a', minimum: 4
+    assert_select '#main .entry', minimum: 1
+    assert_select 'h3', 'Programming Ruby 2.3'
+    assert_select '.price', /\$[,\d]+\.\d\d/
+  end
+
+end
